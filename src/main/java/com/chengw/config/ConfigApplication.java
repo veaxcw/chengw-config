63d1966cc15e9757239c19ec786b2c49d7370398
veaxcw <878899580@qq.com>
2019-11-03 21:21:16 +0800

    新增docker 插件

--- src/main/java/com/chengw/config/ConfigApplication.java
+++ src/main/java/com/chengw/config/ConfigApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * @author chengw
@@ -14,7 +17,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(ConfigApplication.class, args);;
+        final String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+        System.out.println("[----------------------------------------------]");
+        System.out.println("> service started: "+ Arrays.toString(activeProfiles));
+        System.out.println("[----------------------------------------------]");
     }
 
 }
